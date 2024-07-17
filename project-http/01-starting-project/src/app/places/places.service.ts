9c83607bad6fa23eea82010663f31cd09b759f5e
Danila <dannmmaster@gmail.com>
2024-07-17 15:54:49 +0300

    Finish Http Requests chapter

---
+++ project-http/01-starting-project/src/app/places/places.service.ts
@@ -0,0 +1,84 @@
+import { Injectable, inject, signal } from '@angular/core';
+
+import { Place } from './place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap, throwError } from 'rxjs';
+import { ErrorService } from '../shared/error.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlacesService {
+  private errorService = inject(ErrorService);
+  private httpClient = inject(HttpClient);
+  private userPlaces = signal<Place[]>([]);
+
+  loadedUserPlaces = this.userPlaces.asReadonly();
+
+  loadAvailablePlaces() {
+    return this.fetchPlaces(
+      'http://localhost:3000/places',
+      'Something went wrong!'
+    );
+  }
+
+  loadUserPlaces() {
+    return this.fetchPlaces(
+      'http://localhost:3000/user-places',
+      'Something went wrong!'
+    ).pipe(
+      tap({
+        next: (userPlaces) => {
+          this.userPlaces.set(userPlaces);
+        },
+      })
+    );
+  }
+
+  addPlaceToUserPlaces(place: Place) {
+    const prevPlaces = this.userPlaces();
+
+    if (!prevPlaces.some((p) => p.id === place.id)) {
+      this.userPlaces.set([...prevPlaces, place]);
+    }
+
+    return this.httpClient
+      .put('http://localhost:3000/user-places', {
+        placeId: place.id,
+      })
+      .pipe(
+        catchError((error) => {
+          this.userPlaces.set(prevPlaces);
+          this.errorService.showError('Failed to add place to user!');
+          return throwError(() => new Error('Failed to add place to user!'));
+        })
+      );
+  }
+
+  removeUserPlace(place: Place) {
+    const prevPlaces = this.userPlaces();
+    this.userPlaces.set(prevPlaces.filter((p) => p.id !== place.id));
+
+    return this.httpClient
+      .delete(`http://localhost:3000/user-places/${place.id}`)
+      .pipe(
+        catchError((error) => {
+          this.userPlaces.set(prevPlaces);
+          this.errorService.showError('Failed to remove place from user!');
+          return throwError(
+            () => new Error('Failed to remove place from user!')
+          );
+        })
+      );
+  }
+
+  private fetchPlaces(url: string, errorMessage: string) {
+    return this.httpClient.get<{ places: Place[] }>(url).pipe(
+      map((resData) => resData.places),
+      catchError((error) => {
+        console.error(error);
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
+}
---
+++ project-http/01-starting-project/src/app/places/user-places/user-places.component.ts
@@ -0,0 +1,52 @@
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
+
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesComponent } from '../places.component';
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
+
+@Component({
+  selector: 'app-user-places',
+  standalone: true,
+  templateUrl: './user-places.component.html',
+  styleUrl: './user-places.component.css',
+  imports: [PlacesContainerComponent, PlacesComponent],
+})
+export class UserPlacesComponent implements OnInit {
+  isFetching = signal(false);
+  error = signal<string | undefined>(undefined);
+  private placesService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadUserPlaces().subscribe({
+      error: (error) => {
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe({
+      next: () => {
+        console.log('Place removed!');
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+}
